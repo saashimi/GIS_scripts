084e176d893a71d2b38561874331edc37ca3a5a5
Kevin Saavedra <saavedra.kevin@gmail.com>
2017-07-07 09:17:28 -0700

    update student address density script.

--- addr_density.py
+++ addr_density.py
@@ -1,45 +1,59 @@
 # Name: addr_density.py
 # Python 2.7 script
+# Generates a Kernel density map of student addresses.
 
 import arcinfo, arcpy
 arcpy.CheckOutExtension("Spatial")
 
 #set environment settings
-foldername = "C:/Users/kms22/Desktop/py_desktop/address_geocode/"
+foldername = "C:/Users/kms22/Desktop/compy_desktop/data_holding/address_geocode/"
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = foldername
+fileList = arcpy.ListFeatureClasses()
 
-# near analysis will update the fc with new columns
-fc = foldername + "2016_FT.shp"
-in_features = fc
-near_features = "I:/Staff/PSRE/PSRE_Planning/--Resources/CPO_GIS/Projects/StudentAddress/2015/SMSU.shp"
-arcpy.Near_analysis(in_features, near_features)
+for featureClass in fileList:
 
-# insert new field 
-field_name = "DIST_MI"
-field_type = "FLOAT"
-arcpy.AddField_management(in_features, field_name, field_type)
+    # near analysis will update the fc with new columns
+    print "Now processing " + featureClass + "."
+    fc = featureClass
+    in_features = fc
+    near_features = "I:/Staff/PSRE/PSRE_Planning/--Resources/CPO_GIS/Projects/StudentAddress/2015/SMSU.shp"
+    arcpy.Near_analysis(in_features, near_features)
 
-# convert feet to miles
-fields = ["NEAR_DIST", "DIST_MI"]
-FT_IN_MI=5280.
-with arcpy.da.UpdateCursor(fc, fields) as cursor:
-    for row in cursor:
-        row[1] = row[0]/FT_IN_MI
-        cursor.updateRow(row)
+    # insert new field 
+    field_name = "DIST_MI"
+    field_type = "FLOAT"
+    arcpy.AddField_management(in_features, field_name, field_type)
 
-# generate kernel density map
-#SIXTEENTH_MI=330
-population_field = "NONE"
-cellSize = 330 # 1/16 of a mile
-area_unit_scale_factor = "SQUARE_MILES"
-out_cell_values = "DENSITIES"
-method="GEODESIC"
-searchRadius = 168960 # 32 Miles
+    # convert feet to miles
+    fields = ["NEAR_DIST", "DIST_MI"]
+    FT_IN_MI=5280.
+    with arcpy.da.UpdateCursor(fc, fields) as cursor:
+        for row in cursor:
+            row[1] = row[0]/FT_IN_MI
+            cursor.updateRow(row)
 
-outKernelDensity = arcpy.sa.KernelDensity(in_features, population_field, cellSize, searchRadius, area_unit_scale_factor, out_cell_values, method)
-outKernelDensity.save("kdmap")
+    # generate kernel density map
+    population_field = "NONE"
+    cellSize = 330 #1/16 of a mile
+    area_unit_scale_factor = "SQUARE_MILES"
+    out_cell_values = "DENSITIES"
+    method="PLANAR"
+    searchRadius = 5280 #1 Mile
+    saveName = "kd" + featureClass[:-4]
 
+    outKernelDensity = arcpy.sa.KernelDensity(in_features, population_field, cellSize, searchRadius, area_unit_scale_factor, out_cell_values, method)
+    outKernelDensity.save(saveName)
+
+# check in Spatial Analyst license
 arcpy.CheckInExtension("Spatial")
-# import 5, 10, 15, 20-mile symbology
+
+"""
+in_raster = foldername + "kdmap"
+out_raster = "kdmap_proj.tif"
+in_coor_system = 
+out_coor_system = arcpy.SpatialReference(2913) # WKID 2913 StatePlane OR N
+arcpy.ProjectRaster_management (in_raster, out_raster, out_coor_system)
+"""
+print "Script executed successfully."
 
