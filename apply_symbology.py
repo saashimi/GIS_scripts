8108c92e6fba8bcf8221fd8380e2225b22719e72
Kevin Saavedra <saavedra.kevin@gmail.com>
2017-07-12 13:13:12 -0700

    update all scripts.

--- apply_symbology.py
+++
@@ -1,7 +0,0 @@
-import arcpy
-
-#set environment settings
-foldername = "C:/Users/kms22/Desktop/compy_desktop/data_holding/address_geocode/"
-arcpy.env.overwriteOutput = True
-arcpy.env.workspace = foldername
-
