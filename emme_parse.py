0a7d65fd81738799931d9eda061288c2e07e2073
Kevin Saavedra <saavedra.kevin@gmail.com>
2017-12-18 15:32:34 -0800

    Begin work on emme_parse script.

---
+++ emme_parse.py
@@ -0,0 +1,13 @@
+"""
+Import tab-delimited file to separate files for Emme processing
+Author: Kevin Saavedra
+"""
+
+import sys
+
+def main():
+	pass
+	
+
+if __name__ == '__main__':
+    main(sys.argv)
