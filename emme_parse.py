9d5ed7be8a4fe85ece0695e58f9f43f18ae29c14
Kevin Saavedra <saavedra.kevin@gmail.com>
2017-12-18 16:08:53 -0800

    Update emme script.

--- emme_parse.py
+++ emme_parse.py
@@ -2,12 +2,16 @@
 Import tab-delimited file to separate files for Emme processing
 Author: Kevin Saavedra
 """
-
 import sys
+import csv
+import pandas
 
-def main():
-	pass
+def main(argv):
+    # open the tab delim file
+    # For every column:
+        #Extract the column heading as the text file name
+        #For every row in column:
+        	# write to new line in text file.
 	
-
 if __name__ == '__main__':
     main(sys.argv)
