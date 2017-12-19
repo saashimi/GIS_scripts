53cd858e2006a344fbf488f702b41d60c37ede7d
Kevin Saavedra <saavedra.kevin@gmail.com>
2017-12-19 09:19:21 -0800

    Final emme_parse script.

--- emme_parse.py
+++ emme_parse.py
@@ -8,11 +8,13 @@ import numpy as np
 
 def main(filename):
     df = pd.read_csv(filename, sep='\t')
-    df = df.fillna('')
+    df = df.dropna(axis=0, how='all')
     headings = list(df)
-    
+
     for num in range(len(headings)):
         df_temp = df[headings[num]]
+        blank_row = pd.Series([""], index=df[headings[num]])
+        df_final = df_temp.append(blank_row)
         np.savetxt(headings[num]+'.txt', df_temp.values, fmt='%s', newline='\n')
 
 if __name__ == '__main__':
