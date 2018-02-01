cf678725519a6539c12e0361e3f622787dc994e8
Kevin Saavedra <saavedra.kevin@gmail.com>
2018-02-01 08:47:01 -0800

    working first line write behavior.

--- emme/2emme_transit_edit.py
+++ emme/2emme_transit_edit.py
@@ -14,8 +14,13 @@ def dwt_ttf_search_and_replace(ttf_in, dwt_in, line_in):
 def line_as_list(line_in):  
     return filter(None, line_in.rstrip().split(' '))
 
+def unpack_list(list_in):
+    for item in list_in:
+        dest.write(item)
+
 always_write=False # Initial flag state
 inside_stop=False
+header=False
 parse_following_lines=False
 with open('d221.2015_RTP18_pm2', 'r') as src:
     with open('2test_out', 'w') as dest:
@@ -23,29 +28,34 @@ with open('d221.2015_RTP18_pm2', 'r') as src:
             if "a'{0}".format(search_transit_line) in line: 
                 dest.write(line)
                 parse_following_lines=True
+                continue
             
             if search_stop_end in line:
                 line_list = line_as_list(line)
                 parse_following_lines=False
-
+            
             if parse_following_lines:
                 line_list = line_as_list(line)
-                print line_list     
+                unpack_list(line_list)
 
+
+            """
+            # In final stage, we need to write all lines.
             else:
                 dest.write(line)
                 always_print=True
+            """
 
             """
-                else: 
-                    if inside_stop:
-                        parsed_line = dwt_ttf_search_and_replace(ttf, dwt, line)
-                        dest.write(parsed_line)
-                        always_print=True
+            else: 
+                if inside_stop:
+                    parsed_line = dwt_ttf_search_and_replace(ttf, dwt, line)
+                    dest.write(parsed_line)
+                    always_print=True
 
-                    else:
-                        dest.write(line)
-                        always_print=True 
+                else:
+                    dest.write(line)
+                    always_print=True 
             """
 
 src.close()
